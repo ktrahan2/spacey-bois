93707d9d9bf817e19a0a47c295ffa9d3620978d7
Kyle Trahan <68058796+ktrahan2@users.noreply.github.com>
2025-02-22 08:58:48 -0700

    - add mui theme - add global overrides - add styling for main layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,6 +4,8 @@ import {store} from '@root/store/store';
 import {AppRouter} from './router';
 import {ErrorBoundary} from 'react-error-boundary';
 import {MainErrorFallback} from '@root/components/errors/MainErrorFallback';
+import {GlobalStyles, ThemeProvider} from '@mui/material';
+import {theme} from '@root/theme';
 
 export const App = () => {
   return (
@@ -11,7 +13,20 @@ export const App = () => {
       <Suspense fallback={<div>Loading....</div>}>
         <ErrorBoundary FallbackComponent={MainErrorFallback}>
           <Provider store={store}>
-            <AppRouter />
+            <ThemeProvider theme={theme}>
+              <GlobalStyles
+                styles={{
+                  '*': {margin: 0, padding: 0, boxSizing: 'border-box'},
+                  'html, body': {
+                    margin: 0,
+                    padding: 0,
+                    backgroundColor: '#070714',
+                    minHeight: '100vh',
+                  },
+                }}
+              />
+              <AppRouter />
+            </ThemeProvider>
           </Provider>
         </ErrorBoundary>
       </Suspense>
--- src/components/layouts/MainLayout/index.tsx
+++ src/components/layouts/MainLayout/index.tsx
@@ -1,9 +1,10 @@
 import {Outlet} from 'react-router';
+import {MainContainer} from './MainLayout.styles';
 
 export const MainLayout = () => {
   return (
-    <main>
+    <MainContainer>
       <Outlet />
-    </main>
+    </MainContainer>
   );
 };
